c53fc8362c8ade8be0d3bb14075b008c50b79cde
Rene Casco <34440390+renecasco@users.noreply.github.com>
2019-03-11 05:01:06 -0600

    adds model tests for tag and tagging, new relationships to article test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -8,5 +8,7 @@ describe Article, type: :model do
 
   describe "relationshps" do
     it {should have_many(:comments)}
+    it {should have_many(:taggings)}
+    it {should have_many(:tags).through(:taggings)}
   end
 end
